9e95508969eca39febe07c746c8a236a9e74dc85
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-09-06 18:19:55 +0300

    commit

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 //DATE VARIABLES
-// let dayUI = document.querySelector('.day');
-// let monthUI = document.querySelector('.month');
-// let yearUI = document.querySelector('.year');
-// let hourUI = document.querySelector('.hour');
-// let minUI = document.querySelector('.min');
-// let secUI = document.querySelector('.sec');
-// let colonUI = document.querySelector('.colon');
 let dateUI = document.querySelector('.date');
 let timeUI = document.querySelector('.time');
 
-//UI VARIABLES --------------------------------------
+//UI VARIABLES ------------------------------------------------------------------------------
 
 const toggleMode = document.getElementById('toggle-mode');
 const modeText = document.querySelector('.mode-text');
@@ -22,13 +15,58 @@ const mainMenuLinks = document.querySelectorAll('.menu-item');
 const mainNavBar = document.querySelector('.main-nav-bar');
 const aboutTextH2 = document.querySelector('.about-text-h2');
 const aboutText = document.querySelector('.about-text');
+const aboutEmail = document.querySelector('.about-email');
 const color = document.querySelectorAll('.color');
+const activeColor = document.querySelectorAll('.active');
+let chosenColor;
+let mode;
 
 
-//EVENT LISTENERS ----------------------------------
+//EVENT LISTENERS --------------------------------------------------------------------------
 
-//toggle light/dark modes
+//get data from local storage and change classes
+if (localStorage.getItem('mode') === 'light-mode-active') {
+    toggleMode.checked = true;
+    //toggle visibility class name
+    lightDiv.classList.toggle('visibility');
+    //change light/dark mode text color
+    modeText.classList.toggle('dark-color');
+    //change h1s text color
+    h1.forEach((el) => {
+        el.classList.toggle('dark-color')
+    });
+    //change contact me button text color
+    contactBtn.classList.toggle('dark-color');
+    //change time border color
+    timeBox.classList.toggle('dark-color')
+    // //change work in progress color
+    // workInProgress.classList.toggle('dark-color');
+    //change about text color
+    if (aboutTextH2 === null && aboutText === null && aboutEmail === null) {
+
+    } else {
+        aboutTextH2.classList.toggle('dark-color');
+        aboutText.classList.toggle('dark-color');
+        aboutEmail.classList.toggle('dark-color');
+    }
+}
+
+
+
+//toggle light / dark modes 
 toggleMode.addEventListener('click', () => {
+    if (localStorage.getItem('mode') === null) {
+        mode = 'light-mode-active';
+        localStorage.setItem('mode', 'light-mode-active');
+    } else if (localStorage.getItem('mode') === 'dark-mode-active') {
+        localStorage.setItem('mode', 'light-mode-active');
+        mode = 'light-mode-active';
+    } else {
+        localStorage.getItem('mode') === 'light-mode-active';
+        localStorage.setItem('mode', 'dark-mode-active');
+        mode = 'dark-mode-active';
+    }
+
     //toggle visibility class name
     lightDiv.classList.toggle('visibility');
     //change light/dark mode text color
@@ -46,15 +84,14 @@ toggleMode.addEventListener('click', () => {
     //change about text color
     aboutTextH2.classList.toggle('dark-color');
     aboutText.classList.toggle('dark-color');
-
-
+    aboutEmail.classList.toggle('dark-color');
 
     //CHANGE BUTTON BACKGROUND ON HOVER IN LIGHT MODE
 
+});
 
-})
-
-
+//PORTFOLIO COLOR CHANGE 
+//on color click show alert & set color
 color.forEach((color) => {
     color.addEventListener('click', () => {
         if (document.querySelector('.alert')) {
@@ -62,14 +99,20 @@ color.forEach((color) => {
         } else {
             showAlert('Color Selected');
         }
+        chosenColor = color.textContent;
+        setColor('color', color);
+        return chosenColor;
     })
 });
 
+// update colors on window load
+document.addEventListener('DOMContentLoaded', () => {
+    getColor()
+    setColor()
+});
 
-//FUNCTIONS
-
-
-//TIME & DATE FUNCTIONS
+//FUNCTIONS --------------------------------------------------------------------------------------------
+//time and date functions
 function getTime() {
     let day = new Date().getDate();
     let month = new Date().getMonth() + 1;
@@ -105,12 +148,14 @@ function getTime() {
         <span class="hour">${ hour }<span class="not-active">:</span></span><span class="min">${ min }</span><span class="colon"><span class="not-active">:</span></span><span class="sec">${ sec }</span>
     `;
     }
-
-
 };
 
-// ALERT FUNCTION
+//call time function
+setInterval(function () {
+    getTime();
+}, 1000);
 
+// alert function
 function showAlert(message) {
     let colorAlertDiv = document.querySelector('.color-alert');
     let alertDiv = document.createElement('div');
@@ -119,11 +164,32 @@ function showAlert(message) {
     colorAlertDiv.appendChild(alertDiv);
     setTimeout(() => {
         document.querySelector('.alert').remove()
-    }, 3000)
-}
+    }, 1000)
+};
 
-setInterval(function () {
-    getTime();
-}, 1000);
+//local storage color functions NOT COMPLETE
+function getColor() {
+    let color;
+    if (localStorage.getItem('color') === null) {
+        return
+    } else {
+        color = localStorage.getItem('color');
+    }
+    chosenColor = color;
+    return chosenColor;
+};
+
+function setColor() {
+    localStorage.setItem('color', chosenColor);
+    updatePortfolioColor()
+};
+
+function updatePortfolioColor() {
+    activeColor.forEach((color) => {
+        color.style.color = chosenColor;
+    })
+};
+
+
 
 

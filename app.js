d5a3bbb98dde1c019f87f2552964dabac621449e
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-09-07 11:36:04 +0300

    commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const timeBox = document.querySelector('.time');
 const workInProgress = document.querySelector('.work-in-progress');
 const mainMenuLinks = document.querySelectorAll('.menu-item');
 const mainNavBar = document.querySelector('.main-nav-bar');
-const aboutTextH2 = document.querySelector('.about-text-h2');
+let aboutTextH2 = document.querySelector('.about-text-h2');
 const aboutText = document.querySelector('.about-text');
 const aboutEmail = document.querySelector('.about-email');
 const header = document.querySelector('header');
@@ -23,7 +23,15 @@ const color = document.querySelectorAll('.color');
 const activeColor = document.querySelectorAll('.active');
 let logo = document.querySelector('.logo');
 let mainImage = document.querySelector('.main-image');
+let mainText = document.querySelector('.main-text');
 let mainSvg = document.querySelector('.main-image svg');
+let viewProjectsBtn = document.querySelector('.view-projects-btn');
+let contactMeBtn = document.querySelector('.contact-me-btn');
+let btn = document.querySelector('.btn');
+let avatar = document.querySelector('.avatar');
+let project = document.querySelectorAll('.project');
+let projectLinks = document.querySelectorAll('.project-link1');
+let githubLinks = document.querySelectorAll('.github-repo1');
 let chosenColor;
 let mode;
 //create alert div
@@ -136,23 +144,23 @@ function getTime() {
 
     if (lightDiv.classList.contains('visibility')) {
         dateUI.innerHTML = `
-        <span class="day dark-color">${ day } <span class="active">/</span>
-        </span><span class="month dark-color">${ month } <span class="active">/</span>
+        <span class="day dark-color">${ day } <span style="color:${ chosenColor }">/</span>
+        </span><span class="month dark-color">${ month } <span style="color:${ chosenColor }">/</span>
         </span><span class="year dark-color">${ year }</span>
         `;
         timeUI.innerHTML = `
-        <span class="hour dark-color">${ hour }<span class="active colo">:</span></span><span class="min dark-color">${ min }</span><span class="colon"><span class="active colo">:</span></span><span class="sec dark-color">${ sec }</span>
+        <span class="hour dark-color">${ hour }<span class="colo" style="color:${ chosenColor }">:</span></span><span class="min dark-color">${ min }</span><span class="colon"><span class="colo" style="color:${ chosenColor }">:</span></span><span class="sec dark-color">${ sec }</span>
         `;
     } else {
 
         dateUI.innerHTML = `
-        <span class="day">${ day } <span class="active">/</span>
-        </span><span class="month">${ month } <span class="active">/</span>
+        <span class="day">${ day } <span style="color:${ chosenColor }">/</span>
+        </span><span class="month">${ month } <span style="color:${ chosenColor }">/</span>
         </span><span class="year">${ year }</span>
     `;
 
         timeUI.innerHTML = `
-        <span class="hour">${ hour }<span class="not-active">:</span></span><span class="min">${ min }</span><span class="colon"><span class="not-active">:</span></span><span class="sec">${ sec }</span>
+        <span class="hour" style="color:${ chosenColor }">${ hour }<span class="not-active">:</span></span><span class="min" style="color:${ chosenColor }">${ min }</span><span class="colon"><span class="not-active">:</span></span><span class="sec" style="color:${ chosenColor }">${ sec }</span>
     `;
     }
 };
@@ -198,6 +206,9 @@ function setColor() {
 function updatePortfolioColor() {
     activeColor.forEach((color) => {
         color.style.color = chosenColor;
+        viewProjectsBtn.style.backgroundColor = chosenColor;
+        viewProjectsBtn.style.borderColor = chosenColor;
+        contactMeBtn.style.borderColor = chosenColor;
     })
     if (chosenColor === '#2a9d8f') {
         logo.classList.remove('orange-background-color', 'green-background-color', 'pink-background-color');
@@ -206,9 +217,54 @@ function updatePortfolioColor() {
         logo.classList.add('blue-border-bottom');
         header.classList.remove('orange-border-right', 'green-border-right', 'pink-border-right');
         header.classList.add('blue-border-right');
-        mainSvg.classList.add('transparent');
-        mainImage.classList.remove('orange-background-image');
-        mainImage.classList.add('blue-background-image');
+        if (mainSvg === null) {
+        } else {
+            mainSvg.classList.add('transparent');
+        }
+        if (mainImage === null) {
+        } else {
+            mainImage.classList.remove('orange-background-image');
+            mainImage.classList.add('blue-background-image');
+        }
+        if (mainText === null) {
+        } else {
+            mainText.classList.remove('orange-border-color', 'green-border-color', 'pink-border-color');
+            mainText.classList.add('blue-border-color');
+        }
+        if (avatar === null) {
+
+        } else {
+            avatar.classList.remove('orange-border-color', 'green-border-color', 'pink-border-color');
+            avatar.classList.add('blue-border-color');
+        }
+        if (aboutTextH2 === null) {
+
+        } else {
+            aboutTextH2.classList.remove('orange-border-color', 'green-border-color', 'pink-border-color');
+            aboutTextH2.classList.add('blue-color');
+        }
+        if (project === null) {
+
+        } else {
+            project.forEach((proj) => {
+                proj.classList.remove('orange-background-image', 'green-background-image', 'pink-background-image');
+                proj.classList.add('blue-background-image');
+                proj.classList.remove('orange-border-color', 'green-border-color', 'pink-border-color');
+                proj.classList.add('blue-border-color');
+            })
+            projectLinks.forEach((proj) => {
+                proj.classList.remove('orange-border-color', 'green-border-color', 'pink-border-color');
+                proj.classList.add('blue-border-color');
+            })
+            githubLinks.forEach((git) => {
+                git.classList.remove('orange-border-color', 'green-border-color', 'pink-border-color');
+                git.classList.add('blue-border-color');
+            })
+        }
+
+
+
+
     } else if (chosenColor === '#ed8e26') {
         logo.classList.remove('blue-background-color', 'green-background-color', 'pink-background-color');
         logo.classList.add('orange-background-color');
@@ -216,9 +272,51 @@ function updatePortfolioColor() {
         logo.classList.add('orange-border-bottom');
         header.classList.remove('blue-border-right', 'green-border-right', 'pink-border-right');
         header.classList.add('orange-border-right');
-        mainSvg.classList.add('transparent');
-        mainImage.classList.remove('orange-background-image');
-        mainImage.classList.add('orange-background-image');
+        if (mainSvg === null) {
+        } else {
+            mainSvg.classList.add('transparent');
+        }
+        if (mainImage === null) {
+        } else {
+            mainImage.classList.remove('blue-background-image');
+            mainImage.classList.add('orange-background-image');
+        }
+        if (mainText === null) {
+        } else {
+            mainText.classList.remove('blue-border-color', 'green-border-color', 'pink-border-color');
+            mainText.classList.add('orange-border-color');
+        }
+        if (avatar === null) {
+
+        } else {
+            avatar.classList.remove('blue-border-color', 'green-border-color', 'pink-border-color');
+            avatar.classList.add('orange-border-color');
+        }
+        if (aboutTextH2 === null) {
+
+        } else {
+            aboutTextH2.classList.remove('blue-border-color', 'green-border-color', 'pink-border-color');
+            aboutTextH2.classList.add('orange-color');
+        }
+        if (project === null) {
+
+        } else {
+            project.forEach((proj) => {
+                proj.classList.remove('blue-background-image', 'green-background-image', 'pink-background-image');
+                proj.classList.add('orange-background-image');
+                proj.classList.remove('blue-border-color', 'green-border-color', 'pink-border-color');
+                proj.classList.add('orange-border-color');
+            })
+            projectLinks.forEach((proj) => {
+                proj.classList.remove('blue-border-color', 'green-border-color', 'pink-border-color');
+                proj.classList.add('orange-border-color');
+            })
+            githubLinks.forEach((git) => {
+                git.classList.remove('blue-border-color', 'green-border-color', 'pink-border-color');
+                git.classList.add('orange-border-color');
+            })
+        }
+
     } else if (chosenColor === '#b1e84a') {
         logo.classList.remove('blue-background-color', 'orange-background-color', 'pink-background-color');
         logo.classList.add('green-background-color');
@@ -226,9 +324,51 @@ function updatePortfolioColor() {
         logo.classList.add('green-border-bottom');
         header.classList.remove('orange-border-right', 'blue-border-right', 'pink-border-right');
         header.classList.add('green-border-right');
-        mainSvg.classList.add('transparent');
-        mainImage.classList.remove('orange-background-image');
-        mainImage.classList.add('green-background-image');
+        if (mainSvg === null) {
+        } else {
+            mainSvg.classList.add('transparent');
+        }
+        if (mainImage === null) {
+        } else {
+            mainImage.classList.remove('orange-background-image');
+            mainImage.classList.add('green-background-image');
+        }
+        if (mainText === null) {
+        } else {
+            mainText.classList.remove('orange-border-color', 'blue-border-color', 'pink-border-color');
+            mainText.classList.add('green-border-color');
+        }
+        if (avatar === null) {
+
+        } else {
+            avatar.classList.remove('orange-border-color', 'blue-border-color', 'pink-border-color');
+            avatar.classList.add('green-border-color');
+        }
+        if (aboutTextH2 === null) {
+
+        } else {
+            aboutTextH2.classList.remove('orange-border-color', 'blue-border-color', 'pink-border-color');
+            aboutTextH2.classList.add('green-color');
+        }
+        if (project === null) {
+
+        } else {
+            project.forEach((proj) => {
+                proj.classList.remove('orange-background-image', 'blue-background-image', 'pink-background-image');
+                proj.classList.add('green-background-image');
+                proj.classList.remove('blue-border-color', 'green-border-color', 'pink-border-color');
+                proj.classList.add('green-border-color');
+            })
+            projectLinks.forEach((proj) => {
+                proj.classList.remove('orange-border-color', 'blue-border-color', 'pink-border-color');
+                proj.classList.add('green-border-color');
+            })
+            githubLinks.forEach((git) => {
+                git.classList.remove('orange-border-color', 'blue-border-color', 'pink-border-color');
+                git.classList.add('green-border-color');
+            })
+        }
+
     } else if (chosenColor === '#ff87ab') {
         logo.classList.remove('blue-background-color', 'orange-background-color', 'green-background-color');
         logo.classList.add('pink-background-color');
@@ -236,9 +376,50 @@ function updatePortfolioColor() {
         logo.classList.add('pink-border-bottom');
         header.classList.remove('orange-border-right', 'blue-border-right', 'green-border-right');
         header.classList.add('pink-border-right');
-        mainSvg.classList.add('transparent');
-        mainImage.classList.remove('orange-background-image');
-        mainImage.classList.add('pink-background-image');
+        if (mainSvg === null) {
+        } else {
+            mainSvg.classList.add('transparent');
+        }
+        if (mainImage === null) {
+        } else {
+            mainImage.classList.remove('orange-background-image');
+            mainImage.classList.add('pink-background-image');
+        }
+        if (mainText === null) {
+        } else {
+            mainText.classList.remove('orange-border-color', 'blue-border-color', 'green-border-color');
+            mainText.classList.add('pink-border-color');
+        }
+        if (avatar === null) {
+
+        } else {
+            avatar.classList.remove('orange-border-color', 'blue-border-color', 'green-border-color');
+            avatar.classList.add('pink-border-color');
+        }
+        if (aboutTextH2 === null) {
+
+        } else {
+            aboutTextH2.classList.remove('orange-border-color', 'blue-border-color', 'green-border-color');
+            aboutTextH2.classList.add('pink-color');
+        }
+        if (project === null) {
+
+        } else {
+            project.forEach((proj) => {
+                proj.classList.remove('blue-background-image', 'green-background-image', 'orange-background-image');
+                proj.classList.add('pink-background-image');
+                proj.classList.remove('orange-border-color', 'green-border-color', 'blue-border-color');
+                proj.classList.add('pink-border-color');
+            })
+            projectLinks.forEach((proj) => {
+                proj.classList.remove('orange-border-color', 'green-border-color', 'blue-border-color');
+                proj.classList.add('pink-border-color');
+            })
+            githubLinks.forEach((git) => {
+                git.classList.remove('orange-border-color', 'green-border-color', 'blue-border-color');
+                git.classList.add('pink-border-color');
+            })
+        }
 
 
 
@@ -248,7 +429,6 @@ function updatePortfolioColor() {
         // CONTINUE WITH CHANGING COLORS 
     }
 };
-
 
 
 
